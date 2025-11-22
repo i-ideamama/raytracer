2f9e3610c9c718bc3b605df4ff00bde70fcc5537
i-ideamama <advaith.n@gmail.com>
2025-11-22 22:45:26 +0530

    completed till chapter 6.4

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,23 @@
 #include "color.h"
 #include "ray.h"
 #include "vec3.h"
+#include "sphere.h"
+#include <limits.h>
 
 #include <iostream>
 
 auto aspect_ratio = 16.0 / 9.0;
 int image_width = 400;
 
-// color ray_color(const ray &r)
-// {
-//     vec3 unit_direction = unit_vector(r.direction());
-//     auto a = 0.5 * (unit_direction.y() + 1.0);
-//     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
-// }
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
-}
 color ray_color(const ray& r) {
-    if (hit_sphere(point3(0,0,-1), 0.5, r))
-        return color(1, 0, 0);
+    sphere s(point3(0,0,-1) , 0.5);
+    hit_record h;
+    auto t = s.hit(r, 0, INT_MAX, h);
+    if (t > 0.0) {
+        vec3 N = unit_vector(h.normal);  // Use the normal from hit_record
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);    
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
